4539f62d607fdf4407720bd5a07fa4308c257f65
altoidnerd <allenmajs1@gmail.com>
2016-01-22 00:02:03 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bitcoin-bench
-benchmark bitcoin node bootstrapping
+
+![Alt text](http://i.imgur.com/wkuOOLf.png "btc-bench.bash")
+
+
+Someone told me using the default bitcoind debug.log may slow down the test. With this script, you can benchmark bitcoin node bootstrapping.  It writes a neat log file when you launch bitcoind with the script.  
+
+
+### use
+
+Download a bitcoin-core release and change into the directory bitcoin-0.12.0/bin.  Run bench.bash from that location as shown. The logfile will be created in the same directory.
+
+
---
+++ btc-bench.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+startTime=$(date +%Y-%d-%mT%T%z)
+startTimeSeconds=$(date +%s)
+logFile="bitcoin_bench_""$startTimeSeconds"".log"
+nSeconds=2
+currentBlockHeight=394395
+dbCacheMbytes=6000 # 6 GB default
+bitcoindCmd="./bitcoind -dbcache=$dbCacheMbytes &"
+x='echo -e'
+
+$x "$(uname -a)" >> $logFile
+$x "Writing to file: ./$logfile"
+
+a=$startTime; b=$startTimeSeconds; c=$logFile; d=$nSeconds; e=$currentBlockHeight; f=$dbCacheMbytes; g=$bitcoindCmd;
+
+$x "Time:\t\t$a\nUnix Time\t\t$b\nLog File Name:\t\t$c\nRecording:\t\t$g\ntimeout:\t\t$d\nCurrentBlockheight\t\t$e\ndbcahce:\t\t$f bytes\n" >> $logFile
+
+if [[ -a "$HOME/.bitcoin" ]]; then
+	$x "\n\n################\n#\n#\tERROR:\n#\t>>\t$HOME/.bitcoin exists.  This directory may store your wallet data. \n#\t>>\tMake a backup of wallet.dat, remove $HOME/.bitcoin, and try again.\n#\t>>\tAborting now.\n#\n################"
+	exit 1;
+fi
+
+$x "START TIME:\t$startTime" >> "$logFile"
+message="Executing\t$bitcoindCmd\tevery $nSeconds seconds"
+$x $message >> $logFile;
+$x $message
+
+./bitcoind -dbcache=$dbCacheMbytes &
+
+while :
+  do
+    $x "blockcount: $(./bitcoin-cli getblockcount)\tTime: $(date +%Y-%m-%dT%T)\tUnix Time (sec): $(date +%s)"  >> "$logFile"
+    sleep "$nSeconds"
+done
+
+
