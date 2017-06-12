0706251d7188f5b90bcf5df6ca1e26544dbe503a
altoidnerd <allenmajs1@gmail.com>
2017-06-12 15:23:11 -0400

    updates

--- btc-bench.bash
+++ btc-bench.bash
@@ -3,34 +3,79 @@
 startTime=$(date +%Y-%d-%mT%T%z)
 startTimeSeconds=$(date +%s)
 logFile="bitcoin_bench_""$startTimeSeconds"".log"
-# this makes an rpc call every 2 seconds; increase for slower systems
+
+#######################################
+#     $nSeconds sets an rpc call      #
+#     every 2 seconds; increase       #
+#     for slower  systems.            #
+#######################################
+#     6 GB default dbcache size;      #
+#     descrease for older systems     #
+#     with less memory	              #
+#######################################
+
 nSeconds=2 
 currentBlockHeight=394395
-# 6 GB default dbcache size; descrease for older systems with less memory
 dbCacheMbytes=6000 
 bitcoindCmd="./bitcoind -dbcache=$dbCacheMbytes &"
 
-x='echo -e'
-$x "$(uname -a)" >> $logFile
-$x "Writing to file: ./$logfile"
-a=$startTime; b=$startTimeSeconds; c=$logFile; d=$nSeconds; e=$currentBlockHeight; f=$dbCacheMbytes; g=$bitcoindCmd;
-$x "Time:\t\t$a\nUnix Start Time:\t\t$b\nLog File Name:\t\t$c\nRecording:\t\t$g\ntimeout:\t\t$d\nCurrentBlockheight\t\t$e\ndbcahce:\t\t$f MB\n" >> $logFile
+e='echo -e'
 
-# warns users not to proceed with their wallets in the data directory
+$e
+$e
+$e "$(uname -a)" >> $logFile
+$e "Writing to file: ./$logfile"
+$e
+$e
+
+a=$startTime 
+b=$startTimeSeconds 
+c=$logFile 
+d=$nSeconds
+e=$currentBlockHeight 
+f=$dbCacheMbytes
+g=$bitcoindCmd
+
+
+$e				>>	$logfile
+$e "Time:\t\t$a"		>>	$logfile
+$e "Unix Start Time:\t\t$b"	>>	$logfile
+$e "Log File Name:\t\t$c"	>>	$logfile
+$e "Recording:\t\t$g"		>>	$logfile
+$e "timeout:\t\t$d"		>>	$logfile
+$e "CurrentBlockheight\t\t$e"	>>	$logfile
+$e "dbcahce:\t\t$f MB" 		>>	$logFile
+$e				>>	$logfile
+
+#######################################
+# 	warns users not to proceed    #
+# 	with their wallets in the     #
+#      	data directory                #
+#######################################
+
 if [[ -a "$HOME/.bitcoin" ]]; then
-	$x "\n\n################\n#\n#\tERROR:\n#\t>>\t$HOME/.bitcoin exists.  This directory may store your wallet data. \n#\t>>\tMake a backup of wallet.dat, remove $HOME/.bitcoin, and try again.\n#\t>>\tAborting now.\n#\n################"
+	$e
+	$e "\n\n################"
+	$e "\tERROR:\t$HOME/.bitcoin exists."
+	$e "This directory may store your"
+	$e "wallet data. Please make a "
+	$e "backup of wallet.dat, remove "
+	$e "$HOME/.bitcoin, and try again."
+	$e "\n\tAborting now."
+	$e "################"
+	$e
 	exit 1;
 fi
 
-$x "START TIME:\t$startTime" >> "$logFile"
+$e "START TIME:\t$startTime" >> "$logFile"
 message="Executing\t$bitcoindCmd\tevery $nSeconds seconds"
-$x $message >> $logFile;
-$x $message
+$e $message >> $logFile;
+$e $message
 
 ./bitcoind -dbcache=$dbCacheMbytes &
 
 while :
   do
-    $x "blockcount: $(./bitcoin-cli getblockcount)\tTime: $(date +%Y-%m-%dT%T)\tTime Elapsed (sec): $(($(date +%s)-b))\tUnixTime (sec): $(date +%s)"  >> "$logFile"
+    $e "blockcount: $(./bitcoin-cli getblockcount)\tTime: $(date +%Y-%m-%dT%T)\tTime Elapsed (sec): $(($(date +%s)-b))\tUnixTime (sec): $(date +%s)"  >> "$logFile"
     sleep "$nSeconds"
 done
